47b420be7ad1e6943bfed53a1f6671b86bfa6a41
Zbysz-Ko <zib@zajebiscie.net>
2024-01-01 14:43:56 +0100

    Code refactoring

--- double_quotes_spaces.py
+++ double_quotes_spaces.py
@@ -7,27 +7,27 @@ def double_quotes_spaces(path: str, mode: bool = True) -> str:
     Switch @mode, if:
     - True - puts all @path in quotation marks
     - False - puts only nodes w/spaces in quotation marks"""
-    
+
     if ' ' not in path:
         return path
-    
+
     if mode:
         return '"' + path + '"'
 
     path = path.split('\\')
 
-    for i in range(len(path)):
-        if ' ' in path[i]:
-            path[i] = '"' + path[i] + '"'
+    for i, j in enumerate(path):
+        if ' ' in j:
+            path[i] = f'"{j}"'
 
     return '\\'.join(path)
 
-path = r'C:\Program Files\7-zip\Program Files\Program Files\7-zip\Program Fil'
+path2check = r'C:\Program Files\7-zip\Program Files\Program Files\7-zip\Program Fil'
 
-print(double_quotes_spaces(path))
-print(ddouble_quotes_spaces(path, False))
+print(double_quotes_spaces(path2check))
+print(double_quotes_spaces(path2check, False))
 
-path = r'C:\ProgramFiles\7-zip\ProgramFiles'
+path2check = r'C:\ProgramFiles\7-zip\ProgramFiles'
 
-print(double_quotes_spaces(path))
-print(double_quotes_spaces(path, False))
+print(double_quotes_spaces(path2check))
+print(double_quotes_spaces(path2check, False))
--- openfile.py
+++ openfile.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-# Learning Python
-# Collected methods for open and read a file.
+""" Learning Python
+Collected methods for open and read a file.
+"""
 
-import sys
 import time
 
 ### First method
@@ -25,7 +25,7 @@ f.close()
 f = None
 
 try:
-    f = open("openfile-data.txt", encoding="UTF-8")
+    f = open("openfile_data.txt", encoding="UTF-8")
     # Our usual file-reading idiom
     while True:
         line = f.readline()
@@ -36,7 +36,7 @@ try:
         # To make sure it runs for a while
         time.sleep(2)
 except IOError:
-    print("Could not find file openfile-data.txt")
+    print("Could not find file openfile_data.txt")
 except KeyboardInterrupt:
     print("!! You cancelled the reading from the file.")
 finally:
